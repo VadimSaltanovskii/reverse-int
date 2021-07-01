6d5cf5f1c7a2fdb8e699bdedc4fa40fe4f4fb7bd
V.Saltanovskiy <v.saltanovskiy@zmeke.com>
2021-07-01 11:10:04 +0300

    feat: reverse int solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n *= -1
+        return Number(n.toString().split('').reverse().join(''))
+    } else {
+        return Number(n.toString().split('').reverse().join(''))
+    }
 }
